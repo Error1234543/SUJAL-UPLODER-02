7edf1ca1d5c0c218f237c0013fa711d8863be85d
Error1234543 <ptaviyad21@gmail.com>
2025-08-05 23:12:03 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,13 +3,13 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "8118667253"))
+API_ID = int(environ.get("API_ID", "20619533"))
+API_HASH = environ.get("API_HASH", "5893568858a096b7373c1970ba05e296")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8146069942:AAHUml7dOsNQg2B0vEDy7-bXWAzc5pugP7A")
+OWNER = int(environ.get("OWNER", "7447651332"))
 CREDIT = "✿𝐒𝐔𝐉𝐀𝐋✿"
 
-AUTH_USER = os.environ.get('AUTH_USERS', '8118667253').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7447651332').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
